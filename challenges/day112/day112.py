2e24d54b9119e616b18a9bc3c26b05d027fe1fb7
OYanEnrique <enrique4work@gmail.com>
2025-12-24 13:57:47 -0300

    feat: add day 112 solution - Remove Exclamation Mark from End

---
+++ challenges/day112/day112.py
@@ -0,0 +1,15 @@
+"""
+Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi!!"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+"""
+
+def remove(s):
+    return "".join(s.rsplit("!", 1)) if s.rfind("!") > 0 else s
