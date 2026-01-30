838074a708f17a3e54df774fe03e3ff552f571b2
OYanEnrique <enrique4work@gmail.com>
2026-01-30 14:59:41 -0300

    feat: add day141 - Check Exam

---
+++ challenges/day141/day141.py
@@ -0,0 +1,19 @@
+'''
+Description:
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+    Correct answer    |    Student's answer   |   Result         
+ ---------------------|-----------------------|-----------
+ ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+ ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+ ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+ ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+'''
+def check_exam(arr1,arr2):
+    return max(0,sum(4 if a == b else (-1 if b != '' else 0) for a, b in zip(arr1, arr2)))
