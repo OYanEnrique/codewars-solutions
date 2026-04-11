dbc662aa0853a470bdd84348efe07c6a1645cf31
OYanEnrique <enrique4work@gmail.com>
2026-04-11 14:26:19 -0300

    feat: add day207 solution - validate code with simple regex

---
+++ challenges/day207/day207.py
@@ -0,0 +1,9 @@
+'''
+validate code with simple regex:
+Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+You can assume the input will always be a number.
+'''
+import re
+def validate_code(code):
+    return bool(re.fullmatch(r'^[123]\d*$',str(code)))
