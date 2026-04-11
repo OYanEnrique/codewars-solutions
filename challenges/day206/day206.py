0e24d25b85d01d519b88dc51f5a8ff7003dbdbf5
OYanEnrique <enrique4work@gmail.com>
2026-04-11 14:12:27 -0300

    feat: add day206 solution - Grasshopper - Combine strings

---
+++ challenges/day206/day206.py
@@ -0,0 +1,11 @@
+'''
+Grasshopper - Combine strings:
+Combine strings function
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+'''
+def combine_names(first, last):
+    return f"{first} {last}"
