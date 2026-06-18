13c80496e6268fb0243fb61a7cd88ce6f9202f9b
OYanEnrique <enrique4work@gmail.com>
2026-06-18 19:54:26 -0300

    feat: add day255 solution - Area of a Square

---
+++ challenges/day255/day255.py
@@ -0,0 +1,9 @@
+'''
+Area of a Square:
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input.
+
+Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+'''
+import math
+def square_area(a):
+    return ((2 * a)/ math.pi)**2
