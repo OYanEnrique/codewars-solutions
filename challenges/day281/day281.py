0228f6e6211c109a82fef9e81ae2d9892891a812
OYanEnrique <enrique4work@gmail.com>
2026-07-16 15:53:15 -0300

    feat: add day281 solution - Find the middle element

---
+++ challenges/day281/day281.py
@@ -0,0 +1,13 @@
+'''
+Find the middle element:
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+'''
+def gimme(input_array):   
+    return [i for i, n in enumerate(input_array) if input_array[i] == sorted(input_array)[1]][0]
