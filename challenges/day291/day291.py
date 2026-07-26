ed5f071e39e02f311b74229991e251503c7af39d
OYanEnrique <enrique4work@gmail.com>
2026-07-26 20:22:21 -0300

    feat: add day291 solution - Char Code Calculation

---
+++ challenges/day291/day291.py
@@ -0,0 +1,24 @@
+'''
+Char Code Calculation:
+Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+total1 = 656667
+              ^
+total2 = 656661
+              ^
+Then return the difference between the sum of the digits in total1 and total2:
+
+  (6 + 5 + 6 + 6 + 6 + 7)
+- (6 + 5 + 6 + 6 + 6 + 1)
+-------------------------
+                       6
+'''
+def calc(x):
+    base=''.join([str(ord(word)) for word in x])
+    total1=sum([int(num) for num in base])
+    base2=base.replace('7','1')
+    total2=sum([int(num) for num in base2])
+    return total1-total2
