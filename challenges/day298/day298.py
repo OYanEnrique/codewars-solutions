491df131026cf9cfd37618c4d83517651ca6874b
OYanEnrique <enrique4work@gmail.com>
2026-08-02 01:09:42 -0300

    feat: add day298 solution - Color Ghost

---
+++ challenges/day298/day298.py
@@ -0,0 +1,15 @@
+'''
+Color Ghost:
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = Ghost()
+ghost.color  #=> "white" or "yellow" or "purple" or "red"
+'''
+from random import choice
+class Ghost(object):
+    def __init__(self):
+        self.color = choice(["white","yellow","purple","red"])
