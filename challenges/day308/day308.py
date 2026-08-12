ecd9baa751464247c8073ed5a65345cc17f85ea4
OYanEnrique <enrique4work@gmail.com>
2026-08-12 02:20:26 -0300

    feat: add day308 solution -  esreveR

---
+++ challenges/day308/day308.py
@@ -0,0 +1,11 @@
+'''
+esreveR:
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+'''
+def reverse(lst):
+    reverse = list()
+    for i in range(len(lst) - 1, -1, -1):
+        reverse.append(lst[i])
+    return reverse
