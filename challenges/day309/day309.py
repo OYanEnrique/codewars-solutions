eff01bf3620424edcdfc0e13346340e3182d62bf
OYanEnrique <enrique4work@gmail.com>
2026-08-13 05:11:52 -0300

    feat: add day309 solution - Find the Slope

---
+++ challenges/day309/day309.py
@@ -0,0 +1,8 @@
+'''
+Find the Slope:
+'''
+def find_slope(points):
+    try:
+        return str(int((points[3]-points[1])/(points[2]-points[0])))
+    except ZeroDivisionError:
+        return "undefined"
